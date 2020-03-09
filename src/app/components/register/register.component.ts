2a81e1a56be203a63b242700dac119c5512bd188
Dagbouj-Hatem <dagboujhatem@gmail.com>
2020-03-09 18:27:06 +0100

    register component is updated

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {ApiService} from '../../services/api.service';
+import {Router} from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+// import custom validator to validate that password and confirm password fields match
+import {MustMatch} from '../../_helpers/must-match.validator';
 
 @Component({
   selector: 'app-register',
@@ -8,32 +12,67 @@ import {ApiService} from '../../services/api.service';
 })
 export class RegisterComponent implements OnInit {
 
-  fname = '' ;
-  email = '';
-  password = '';
+  registerForm: FormGroup;
+  submitted = false;
   registred = false;
+  errorsFromServer = false;
+  errorsMessage = '';
 
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService,
+              private router: Router,
+              private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validator: MustMatch('password', 'confirmPassword')
+    });
   }
 
+  // convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls; }
+
   register() {
-    this.api.register({
-      username: this.fname,
-      email: this.email,
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    const user = {
+      username: this.registerForm.get('username').value,
+      email: this.registerForm.get('email').value,
       emailVerified: true,
-      password: this.password
-    }).subscribe(
+      password: this.registerForm.get('password').value
+    };
+
+    this.api.register(user).subscribe(
       response => {
         this.registred = true;
-        this.fname = '';
-        this.email = '';
-        this.password = '';
+        this.registerForm.reset();
       },
-      error => {
-        console.log(error);
+      errorResponse => {
+        // if Validation Error
+        if (errorResponse.status == 422) {
+          this.errorsFromServer = true;
+          this.showErrorsMessage(errorResponse.error);
+        }
       });
+  }
+  showErrorsMessage( error) {
+    if(error.error.details.messages !== undefined && error.error.details.messages !== null) {
+      if(error.error.details.messages.username !== undefined) {
+        this.errorsMessage = 'Username already exists';
+      }
+      if(error.error.details.messages.email !== undefined) {
+        this.errorsMessage = error.error.details.messages['email'];
+      }
+    }
   }
 
 }
